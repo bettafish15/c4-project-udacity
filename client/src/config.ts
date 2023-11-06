8983210a604c875d6bd2fd72a3cc406ccd25f4c8
bettafish15 <bettafish6@gmail.com>
2023-11-06 22:34:59 +0700

    initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'o1b18qrfwd'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-jn42im6v3lb6oqqq.us.auth0.com',            // Auth0 domain
+  clientId: '4emfwSMwTg8sVJwsF3sl7ThDkKSnBZYM',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
